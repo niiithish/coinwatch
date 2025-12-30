b6cbe04d20b497844c846ca985d4ec2c3e124c13
Nithish Gowda <niiithish@gmail.com>
2025-12-30 11:44:36 +0530

    feat: Introduce new UI components (skeleton, sheet, tooltip, sidebar), implement email sending, and add a `useMobile` hook.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,25 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+import { SidebarHeader } from "@/components/ui/sidebar";
+import Image from "next/image";
 const Dashboard = () => {
   return (
     <div>
-      Hello World!
+      <SidebarProvider>
+        <Sidebar>
+          <SidebarHeader className="border-b">
+            <div className="flex flex-row gap-3 p-2">
+              <Image src="logo.svg" alt="Coin Mantra" width={26} height={26} />
+              <h1 className="text-xl font-bold">CoinWatch</h1>
+            </div>
+          </SidebarHeader>
+          <SidebarContent>Home</SidebarContent>
+        </Sidebar>
+      </SidebarProvider>
+      Hello World
     </div>
   );
 };
--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { signOutAction } from "@/app/actions/auth";
 import Link from "next/link";
 import Image from "next/image";
@@ -9,35 +9,39 @@ import { HugeiconsIcon } from "@hugeicons/react";
 import { ThemeSwitch } from "./theme-switch";
 
 export default async function Header() {
-        const session = await auth.api.getSession({
-          headers: await headers(),
-        });
-        return (
-        <header className="flex px-8 py-4 border-b items-center justify-between">
-          <Link href="/" className="flex items-center gap-2">
-            <Image src="logo.svg" alt="Coin Mantra" width={26} height={26} />
-            <h1 className="text-xl font-bold">CoinMantra</h1>
-          </Link>
-          <div className="flex gap-4 items-center">
-            <ThemeSwitch/>
-          {session ? (
-            <div className="flex gap-4">
-              <Button variant="secondary">Go to Dashboard <HugeiconsIcon icon={ArrowRightIcon}/></Button>
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  return (
+    <header className="flex px-8 py-4 border-b items-center justify-between">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src="logo.svg" alt="Coin Mantra" width={26} height={26} />
+        <h1 className="text-xl font-bold">CoinWatch</h1>
+      </Link>
+      <div className="flex gap-4 items-center">
+        <ThemeSwitch />
+        {session ? (
+          <div className="flex gap-4">
+            <Link href="/dashboard">
+              <Button variant="secondary">
+                Go to Dashboard <HugeiconsIcon icon={ArrowRightIcon} />
+              </Button>
+            </Link>
             <form action={signOutAction}>
               <Button type="submit">Sign Out</Button>
             </form>
-            </div>
-          ) : (
-            <div className="flex gap-2">
-              <Link href="/login">
-                <Button variant="outline">Sign In</Button>
-              </Link>
-              <Link href="/sign-up">
-                <Button>Sign Up</Button> 
-              </Link>
-            </div>
-          )}
           </div>
-        </header>
-        )
+        ) : (
+          <div className="flex gap-2">
+            <Link href="/login">
+              <Button variant="outline">Sign In</Button>
+            </Link>
+            <Link href="/sign-up">
+              <Button>Sign Up</Button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 }
